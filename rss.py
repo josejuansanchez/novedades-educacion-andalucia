5600f8b0061a90ebb5c6df0dbbff3ba0108246de
José Juan Sánchez Hernández <josejuansanchez@Las-Salinas.local>
2017-11-13 00:28:27 +0100

    Added new method to control the published news

--- rss.py
+++ rss.py
@@ -2,10 +2,10 @@
 import json
 import sqlite3
 import sys
+import threading
 
 import requests
 from bs4 import BeautifulSoup
-
 
 # Read configuration file
 with open('config.json', 'r') as f:
@@ -30,7 +30,7 @@ def get_news_from_rss(url):
     return news
 
 def save_news_in_db(news):
-    db = sqlite3.connect('data/novedades.sqlite')
+    db = sqlite3.connect(config['database'])
     cursor = db.cursor()
 
     for new in news:
@@ -56,6 +56,7 @@ def get_news_from_db(query):
     news = []
     for index,row in enumerate(rows):
         new = {
+            'id': row[0],
             'title_and_link': '<b>' + str(index + 1) + '</b>. ' + row[1] + '\n\n' + row[2]
             #'title': row[1],
             #'link': row[2],
@@ -67,7 +68,7 @@ def get_news_from_db(query):
     return news
 
 def get_today_news():
-    query = "SELECT * FROM news WHERE date >= date('now')"
+    query = "SELECT * FROM news WHERE date >= date('now') AND published = 0"
     return get_news_from_db(query)
 
 def get_last_news():
@@ -78,10 +79,21 @@ def get_all_news():
     query = "SELECT * FROM news ORDER BY date DESC"
     return get_news_from_db(query)
 
+def set_new_as_published(id):
+    db = sqlite3.connect(config['database'])
+    cursor = db.cursor()
+    cursor.execute("UPDATE news SET published = 1 WHERE id = ?", (id,))
+    db.commit()
+    db.close()
+
 def main():
+
+    threading.Timer(3600, main).start()
+
     for url in config['urls']:
         news = get_news_from_rss(url)
         save_news_in_db(news)
+        print('Test')
 
 if __name__ == '__main__':
     main()
