381471b56c130e8840699d9b093dd210fe17da1a
José Juan Sánchez <josejuan.sanchez@gmail.com>
2018-01-09 13:39:31 +0100

    Removed cherrypy web server

--- educabot/website.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import cherrypy
-
-class SimpleWebsite(object):
-    @cherrypy.expose
-    def index(self):
-        return "<h1>EducaBot is running...</h1>"
